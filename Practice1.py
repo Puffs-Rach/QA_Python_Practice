10ab2e16a529924a0358f10f02173ca6c3de136b
Puffs-Rach <r.a.bush27@gmail.com>
2024-04-22 14:51:59 +0100

    First Lab

--- Practice1.py
+++ Practice1.py
@@ -1,1 +1,11 @@
+length = "50"
+length = int(length)
 
+width = "40"
+width =int(width)
+
+perimeter = 2 * (length + width)
+print("Perimeter of the rectangle:", perimeter)
+
+area = length * width
+print("Area of the rectangle", area)
