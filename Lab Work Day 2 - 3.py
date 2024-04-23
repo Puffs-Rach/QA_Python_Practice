2d28348e73f25b747a99b729ec6f07bba06b0462
Puffs-Rach <r.a.bush27@gmail.com>
2024-04-23 16:35:32 +0100

    Lab work 3

--- Lab Work Day 2 - 3.py
+++ Lab Work Day 2 - 3.py
@@ -1,3 +1,26 @@
+# if 
+
+age = int(input("Enter your age: "))
+
+if age >= 18:
+    print("You are in category A")
+
+if age >= 16:
+    print("You are in category B")
+
+if age <= 16:
+    print("You are in category C")
+
+# Using a elif
+
+age = int(input("Enter your age: "))
+
+if age >= 18:
+    print("You are in category A")
+elif age >= 16:
+    print("You are in category B")
+else:
+    print("You are in category C")
 
 # Calculator
 
@@ -16,3 +39,27 @@ if operator in ('1', '2'):
     elif operator == '2':
         print(number, "-", second_number, "=", number + second_number)
 
+# Calculate exam grades
+
+student_grade = int(input("Please enter student score: "))
+
+# Pythagoras's Theorem
+
+print("1. Find length of A given B and C")
+print("2. Find the length of B given A and C")
+print("3. Find the length of C given A and B")
+
+Option_of_Operation = input("Enter which you want: ")
+
+A = int()
+B = int()
+C = int()
+
+A = 2 * (length + width)
+print("Perimeter of the rectangle:", perimeter)
+
+B = length * width
+print("Area of the rectangle", area)
+
+C = 2 ** (A + B)
+print("The length of sides")
