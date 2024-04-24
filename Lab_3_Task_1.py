f2c2be86460f2fd82cc65167bda6e83904c63c37
Puffs-Rach <r.a.bush27@gmail.com>
2024-04-24 15:03:50 +0100

    Re-naming

---
+++ Lab_3_Task_1.py
@@ -0,0 +1,11 @@
+# Lab 3 Task 1
+
+count = 1
+while count <= 100:
+    square_number ** 2
+    print(f"Number: {count}, Square: {square_number}")
+    
+    if square_number > 2000:
+        break
+    
+    count += 1
