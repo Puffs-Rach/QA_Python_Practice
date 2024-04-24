2fc584539445c8c77a274e8b2e795844c63e9034
Puffs-Rach <r.a.bush27@gmail.com>
2024-04-24 14:58:04 +0100

    Tidying Up

--- WIP_Lectures_Practice.py
+++ WIP_Lectures_Practice.py
@@ -23,8 +23,6 @@ else:
 
 x = int(input(70))
 
-
-
 weigt = float(input)
 
 
@@ -246,3 +244,135 @@ print(x.format(name))
 
 # list comprehension
 - previous labs 5 and 6, extra functions challeges if time. rock paper scissors app (if time but tomorrow as well)
+
+# While loop
+
+count = 0
+while count < 5:
+    name = input("Enter a name: ")
+    print(name + " is great")
+    count += 1
+
+# for loop
+
+for count in range(5):
+    name = input("Enter a name: ")
+    print(name + " is great")
+
+# list comprehension
+
+# Collect names using list comprehension
+names = [input("Enter a name: ") for count in range (5)]
+
+# Print each name followed by "is great" on separate lines
+for name in names:
+    print(name + " is great")
+
+# list comprehension 1 line of code only
+
+[print(input("Enter a name: ") + " is great") for count in range(5)]
+
+[print(f"{name} is great!") for name in [input("enter a name") for x in range(5)]]
+
+rewrite without any if statemetns
+or any inbuilf function like max()
+
+# If number is bigger than other number then print larger number
+num = 10
+num1 = 20
+
+if num > num1:
+    print(num)
+else:
+    print(num1)
+
+result (num - num1) * (num1 > num) + num1
+
+# number is greater than num1 so that is false. so now is 0. now we add num1 is greater than 1 times num1.
+largest = num * (num > num1) + num1 * (num1 > num)
+
+print(largest)
+
+
+budget = input
+shakes = {"1": (flavour, 3),}
+
+while True:
+    
+
+length = 50
+
+width = 40
+
+perimeter = 2 * (length + width)
+print("Perimeter of the rectangle:", perimeter)
+
+area = length * width
+print("Area of the rectangle", area)
+
+books = {"author1": ["book1", "book2"], "author2": ["book3", "book 4"]}
+
+
+books = {"JK Rowling": ["Goblet of Fire", "Deathly Hallows"], "CS Lewis": ["Lion, Witch, Wardrobe", "Prince Caspian"]}
+
+print(books["JK Rowling"])
+print(books.get("JK Rowlinggg", "not-found"))
+
+x = int(input("70"))
+if x >= 85:
+    print("distinction")
+elif x >= 65:
+    print("pass")
+else:
+    print("Fail")
+    
+
+
+deposit = 100
+password = "password"
+
+if 0 < deposit <=100 or password == "password":
+    print(f"thankyou for deposit of £{deposit}.")
+else:
+    print("failed to deposit")
+
+if not 0 < deposit <=100 or password != "password":
+    print("failed to deposit")
+else:
+    print("ok")
+    
+# in and not in
+# () is a tuple
+
+name = "root"
+if name in ("root", "admin", "user"):
+    print("invalid username")
+else:
+    print("accepted")
+    
+if name not in ("root", "admin", "user"):
+    print("accepted")
+
+# conversion is a new variable
+# f is an f string, stands for format, format then calls the variable in the string. used instead of ending a string and adding a plus.
+
+weight = float(input("Enter weight: "))
+unit = input("KGS or LBS: ")
+
+if unit.upper() == "KGS":
+    conversion = weight / 0.45
+    print(f'your weight in pounds is {conversion}')
+
+elif unit.upper() == "LBS":
+    conversion = weight * 0.45
+    print(f"your weight in Kilo is {conversion}")
+    KGS = float(input("Enter weight: "))
+else:
+    print("invalid unit entered")
+    
+
+try:
+    weight = float(input("enter weight: "))
+except ValueError:
+    print("invalid input. Please enter a numeric value.")
+
