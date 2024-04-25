4479a0f2ecfa4cc810203ff4fa83844ed9b7563f
Puffs-Rach <r.a.bush27@gmail.com>
2024-04-25 10:32:08 +0100

    Updates

--- Lab_6.py
+++ Lab_6.py
@@ -9,8 +9,15 @@ def getIncomeTax(salary):
         'percentage_of_tax_fourty_five': 0.45
     }
 
-   
+result = 0
+
+if salary <= tax_rates['Personal_Allowance']:
+    result = (tax_rates['percentage_of_tax_zero'])
+elif salary <= tax_rates['first_band']:
+    result = (tax_rates['Personal_Allowance']) * (tax_rates['percentage_of_tax_twenty'])
+elif salary <= (tax_rates['second_band']):
+    result = (tax_rates[])
 
 
-
-return getIncomeTax
+salary = int(input("What is your salary?: £ "))
+print getIncomeTax
--- WIP_Lectures_Practice.py
+++ WIP_Lectures_Practice.py
@@ -376,3 +376,19 @@ try:
 except ValueError:
     print("invalid input. Please enter a numeric value.")
 
+
+# Dictionaries vs Lists
+# This is a list inside a function. This function check_password, contains a list of common_assword which stores some common passwords as strings.
+
+def check_password():
+    common_passwords = [
+        "password"
+        "password123"
+        "1234556789"
+    ]
+
+user_password = input("What is your password? ")
+if user_password in common_passwords:
+    print(f"Use a stronger password '{user_password} is not safe enough.")
+else:
+    print("Password is strong.")
