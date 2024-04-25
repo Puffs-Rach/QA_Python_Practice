72d2854225fadd288ccae375e9d53a43c489e3d2
Puffs-Rach <r.a.bush27@gmail.com>
2024-04-25 10:02:41 +0100

    Lab 6

---
+++ Lab_6.py
@@ -0,0 +1,16 @@
+def getIncomeTax(salary):
+    tax_rates = {
+        'Personal_Allowance': 11850,
+        'first_band': 34500,
+        'second_band': 150000,
+        'percentage_of_tax_zero': 0,
+        'percentage_of_tax_twenty': 0.20,
+        'percentage_of_tax_fourty': 0.40,
+        'percentage_of_tax_fourty_five': 0.45
+    }
+
+   
+
+
+
+return getIncomeTax
